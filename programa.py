bbbb184db87cd2769cbc752eed46ba1670a2cfdd
Joel <JoelVIctor1746@gmail.com>
2021-07-04 12:53:28 -0300

    Organização de questões

--- programa.py
+++ programa.py
@@ -7,12 +7,11 @@ from pprint import pprint
 from copy import copy
 
 
-def draw_poli(ax, color, poli: Poli):
+def draw_poli(ax, color, poli: Poli, limite_max: int = 1, titulo: str="Desenho"):
 
     poli.calcular_estruturas() # calcular arestas/faces
 
     for linha in poli.arestas:
-        print(linha)
         ax.plot(
             [linha[0][0], linha[1][0]],
             [linha[0][1], linha[1][1]],
@@ -29,27 +28,41 @@ def draw_poli(ax, color, poli: Poli):
         )
     )
 
-    ax.scatter(0,0,1, c="red")
-    ax.scatter(0, 1, 0, c="green")
-    ax.scatter(1, 0, 0, c="blue")
+    # Marcadores dos eixos
+    # ax.scatter(0,0,1, c="gray", marker='^')
+    # ax.scatter(0, 1, 0, c="gray", marker='^')
+    # ax.scatter(1, 0, 0, c="gray", marker='^')
 
-    ax.scatter(*poli.origem, c=color[0])
+    ax.scatter(*poli.origem, c=color[0], marker='^')
 
-
-def plot(ax):
-
-    ax.set_zlim3d(-6, 6)
-    ax.set_xlim3d(-6, 6)
-    ax.set_ylim3d(-6, 6)
-
-    ax.set_title("Cenário 1")
+    ax.set_title(titulo)
     ax.set_xlabel("Eixo Y")
     ax.set_ylabel("Eixo X")
     ax.set_zlabel("Eixo Z")
 
-    ax.plot([1.2, -1.2], [0, 0], [0, 0], color="Black", alpha=0.4)
-    ax.plot([0, 0], [1.2, -1.2], [0, 0], color="Black", alpha=0.4)
-    ax.plot([0, 0], [0, 0], [1.2, -1.2], color="Black", alpha=0.4)
+    ax.set_zlim3d(-limite_max, limite_max)
+    ax.set_xlim3d(-limite_max, limite_max)
+    ax.set_ylim3d(-limite_max, limite_max)
+
+    ax.plot([limite_max + 0.2, -limite_max - 0.2], [0, 0], [0, 0], color="Black", alpha=0.4)
+    ax.plot([0, 0], [limite_max + 0.2, -limite_max - 0.2], [0, 0], color="Black", alpha=0.4)
+    ax.plot([0, 0], [0, 0], [limite_max + 0.2, -limite_max - 0.2], color="Black", alpha=0.4)
+
+
+def plot(ax):
+    #
+    # ax.set_zlim3d(-6, 6)
+    # ax.set_xlim3d(-6, 6)
+    # ax.set_ylim3d(-6, 6)
+    #
+    # ax.set_title("Cenário 1")
+    # ax.set_xlabel("Eixo Y")
+    # ax.set_ylabel("Eixo X")
+    # ax.set_zlabel("Eixo Z")
+    #
+    # ax.plot([1.2, -1.2], [0, 0], [0, 0], color="Black", alpha=0.4)
+    # ax.plot([0, 0], [1.2, -1.2], [0, 0], color="Black", alpha=0.4)
+    # ax.plot([0, 0], [0, 0], [1.2, -1.2], color="Black", alpha=0.4)
 
     # plt.axis('off')
     # plt.grid(b=None)
@@ -57,58 +70,76 @@ def plot(ax):
 
 # def camera(camera_pos: tuple=(0,0,0), fuga_pos: tuple=(0,0,0)):
 #
-cubo = paralelepipedo = piramide = tronco = None
 
-def primeira_questao():
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(1, 2, 1, projection="3d")
+def cria_objetos():
 
     # cubo : 1,5
     cubo = Cubo.from_arestas(x=1.5, y=1.5, z=1.5)
     cubo.origem = (1.5/2, 1.5/2, 0)
     cubo.translacao((0,0,0))
-    draw_poli(ax=ax, poli=cubo, color=("blue", 0.1))
 
-    ax = fig.add_subplot(1, 2, 2,projection='3d')
     # paralelepipedo : 1.5, 5, 2.5
     paralelepipedo = Cubo.from_arestas(x=1.5, y=5, z=2.5)
     paralelepipedo.origem = (0,5/2,0)
     paralelepipedo.translacao((0,0,0))
-    draw_poli(ax=ax, poli=paralelepipedo, color=("red", 0.1))
-
-    plot(ax)
 
     # piramide : b=2 a=3
-    # piramide = Piramide.from_arestas(2, 2, 3)
-    # piramide.origem = (1,1, 0)
-    # piramide.translacao((0,0,0))
-    # piramide.rotacao(45, "z")
-    #
+    piramide = Piramide.from_arestas(2, 2, 3)
+    piramide.origem = (1,1, 0)
+    piramide.translacao((0,0,0))
+    piramide.rotacao(45, "z")
+
     # # tronco : b=3 1.3 a=2.5
-    # tronco = PiramideTronco.from_arestas(x_base=3, y_base=3, z=2.5, x_superior=1.3, y_superior=1.3)
-    # tronco.origem = (3/2,3/2,0)
-    # tronco.translacao((0,0,0))
+    tronco = PiramideTronco.from_arestas(x_base=3, y_base=3, z=2.5, x_superior=1.3, y_superior=1.3)
+    tronco.origem = (3/2,3/2,0)
+    tronco.translacao((0,0,0))
 
+    return cubo, paralelepipedo, piramide, tronco
 
+def segunda_questao():
+    fig = plt.figure(figsize=(10, 10))
 
-    # def segunda_questao():
-    #     # octante 1
-    #     cubo.translacao((1.5, 1.5, 0))
-    #     piramide.translacao((3.5 , 3.5, 0))
-    #
-    #     # octante 2
-    #     paralelepipedo.translacao((-2, -3, 0))
-    #     tronco.translacao((-4, -3, 0))
-    #
-    #
-    # segunda_questao()
-    # draw_poli(ax=ax, poli=cubo, color=("blue", 0.1))
-    # draw_poli(ax=ax, poli=piramide, color=("gray", 0.1))
-    # draw_poli(ax=ax, poli=tronco, color=("green", 0.1))
-    # plot()
+    cubo, paralelepipedo, piramide, tronco = cria_objetos()
+
+    # octante 1
+    cubo.translacao((1.5, 1.5, 0))
+    piramide.translacao((3.5 , 3.5, 0))
+
+    # octante 2
+    paralelepipedo.translacao((-2, -3, 0))
+    tronco.translacao((-4, -3, 3))
+
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(0,0,0, c="black")
+
+    draw_poli(ax=ax, poli=cubo, color=("blue", 0.1))
+    draw_poli(ax=ax, poli=paralelepipedo, color=("red", 0.1))
+    draw_poli(ax=ax, poli=piramide, color=("orange", 0.1))
+    draw_poli(ax=ax, poli=tronco, color=("green", 0.1), limite_max=6, titulo="Cenário Questão 2")
+    plot(ax)
+
+def primeira_questao():
+    fig = plt.figure(figsize=(10, 10))
+
+    cubo, paralelepipedo, piramide, tronco = cria_objetos()
+
+    ax = fig.add_subplot(2, 2, 1, projection="3d")
+    draw_poli(ax=ax, poli=cubo, color=("blue", 0.1), limite_max=2, titulo="Cubo")
+    ax = fig.add_subplot(2, 2, 2, projection='3d')
+    draw_poli(ax=ax, poli=paralelepipedo, color=("red", 0.1), limite_max=5.5, titulo="Paralelepipedo")
+    ax = fig.add_subplot(2, 2, 3, projection='3d')
+    draw_poli(ax=ax, poli=piramide, color=("orange", 0.1), limite_max=3.5, titulo="Piramide")
+    ax = fig.add_subplot(2, 2, 4, projection='3d')
+    draw_poli(ax=ax, poli=tronco, color=("green", 0.1), limite_max=3.5, titulo="Tronco")
+    plot(ax)
+
+def terceira_questao():
+    pass
 
 if __name__ == "__main__":
-    primeira_questao()
-    # segunda_questao()
-    # main()
+    # primeira_questao()
+    segunda_questao()
+
+
+
 
